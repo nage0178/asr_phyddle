fbbf0befb4ef0bd2210c698fdccf3e8c6aaaa4a9
Anna Nagel <nagelann000@gmail.com>
2025-10-23 07:53:20 -0500

    More processing scripts'

---
+++ 4_tip/runAllSummary.sh
@@ -0,0 +1,7 @@
+size=2500
+tips=4
+cd 3_bin
+/storage/anna/asr_phyddle/process.sh $tips $size
+/storage/anna/asr_phyddle/parseParallel.sh $tips $size
+/storage/anna/asr_phyddle/mlParseParallel.sh $tips $size 
+cd ../
