897b2e71fdbd110346aae98259c360b149e67e80
Anna Nagel <nagelann000@gmail.com>
2025-10-23 09:30:27 -0500

    Script to run summaries

--- 4_tip/runAllSummary.sh
+++ 4_tip/runAllSummary.sh
@@ -1,7 +1,17 @@
 size=2500
 tips=4
+
+cd 3_bin
+../fixOrder.sh
+./sum.sh
+
+cd ../8_cat
+../fixOrder.sh
+
+cd ../
+
 cd 3_bin
 /storage/anna/asr_phyddle/process.sh $tips $size
 /storage/anna/asr_phyddle/parseParallel.sh $tips $size
-/storage/anna/asr_phyddle/mlParseParallel.sh $tips $size 
+#/storage/anna/asr_phyddle/mlParseParallel.sh $tips $size 
 cd ../
